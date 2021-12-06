6688d79c5251721ff75b3dfdf59346eec1e04653
mamadou42 <momocoly42@gmail.com>
2021-12-06 19:54:52 +0000

    second commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoibW9tbzQyIiwiYSI6ImNrd3V2amtlczF0emgyb3BtZ3R6eDE2cnMifQ.iufG3ETnLGGBbEAj8a1X0w",
+  },
+};
